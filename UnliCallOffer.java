dfdfd68df17bb285efe947cd8f01079684b4e9bf
Sharon Grace T. Hangaan <127215110+SG-Hangaan@users.noreply.github.com>
2024-04-18 10:18:15 +0800

    Add files via upload

---
+++ UnliCallOffer.java
@@ -0,0 +1,6 @@
+package TelcoPromo;
+
+public interface UnliCallOffer {
+    public String showUnliCallsTextOffer(String telcoName, boolean unliCallText);
+    public String showUnliCallsTextOffer(TelcoSubscription telco);
+}
---
+++ UsagePromo.java
@@ -0,0 +1,6 @@
+package TelcoPromo;
+
+public interface UsagePromo {
+    public String showAllowance(String telcoName, double money);
+    public String showAllowance(TelcoSubscription telco);
+}
